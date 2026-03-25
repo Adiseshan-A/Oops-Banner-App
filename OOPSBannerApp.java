1a45610e245923e3aa56b59e58c7e3a2048ef808
Adiseshan-A <adiseshanclt@gmail.com>
2026-03-25 09:06:46 +0530

    UC8:Use Map for Character Patterns and Render via Function

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,51 +1,58 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class OOPSBannerApp {
 
-    static class CharacterPatternMap {
+    static HashMap<Character, String[]> map = new HashMap<>();
 
-        static HashMap<Character, String[]> map = new HashMap<>();
+    static {
 
-        static {
-            map.put('O', new String[]{
-                " *** ",
-                "*   *",
-                "*   *",
-                "*   *",
-                " *** "
-            });
+        map.put('O', new String[]{
+            " *** ",
+            "*   *",
+            "*   *",
+            "*   *",
+            " *** "
+        });
 
-            map.put('P', new String[]{
-                "**** ",
-                "*   *",
-                "**** ",
-                "*    ",
-                "*    "
-            });
+        map.put('P', new String[]{
+            "**** ",
+            "*   *",
+            "**** ",
+            "*    ",
+            "*    "
+        });
 
-            map.put('S', new String[]{
-                " ****",
-                "*    ",
-                " *** ",
-                "    *",
-                "**** "
-            });
-        }
+        map.put('S', new String[]{
+            " ****",
+            "*    ",
+            " *** ",
+            "    *",
+            "**** "
+        });
+
     }
 
     public static void main(String[] args) {
 
-        String word = "OOPS";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter word: ");
+        String word = sc.nextLine().toUpperCase();
 
         for (int i = 0; i < 5; i++) {
 
             for (char c : word.toCharArray()) {
-                System.out.print(CharacterPatternMap.map.get(c)[i] + "  ");
+
+                if (map.containsKey(c))
+                    System.out.print(map.get(c)[i] + "  ");
+
             }
 
             System.out.println();
         }
 
+        sc.close();
     }
 
 }
